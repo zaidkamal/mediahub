f3f9c0329d1fe770d7b45b3054291b9d51d8aa4b
AlFaisal ElDajani <faisal.dajani@gmail.com>
2012-02-03 23:00:15 +0200

    Adding video#create view file. Adding loadVideo method to video controller

--- protected/controllers/VideoController.php
+++ protected/controllers/VideoController.php
@@ -1,5 +1,7 @@
 <?php
 class VideoController extends Controller {
+  private $_video = null;
+  
   public function actionIndex() {
     $user = Yii::app()->user->load();
     $videos = $user->videos();
@@ -7,7 +9,13 @@ class VideoController extends Controller {
   }
   
   public function actionView() {
-    
+    $video = $this->loadVideo();
+    $this->render('view', array('video' => $video));
+  }
+  
+  public function actionCreate() {
+    $video = new Video();
+    $this->render('create', array('video' => $video));
   }
   
   public function actionUpdate() {
@@ -16,5 +24,17 @@ class VideoController extends Controller {
   
   public function actionDelete() {
     
+  }
+  
+  protected function loadVideo() {
+    if (is_null($this->_video)) {
+      $this->_video = Video::model()->findByAttributes(array('id' => $_REQUEST['id'], 'user_id' => Yii::app()->user->id));
+      
+      if (is_null($this->_video)) {
+        throw new CHttpException(404, 'This video does not exist');
+      }
+    }
+    
+    return $this->_video;
   }
 }
