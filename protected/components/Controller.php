cdf2a76b61c1312f6bb1a05111b1c8373caf7521
AlFaisal ElDajani <faisal.dajani@gmail.com>
2012-02-04 22:12:35 +0200

    Disabling anonymous access to all controllers.

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -1,6 +1,14 @@
 <?php
 class Controller extends CController {
-	public $layout = '//layouts/column1';
-	public $menu = array();
-	public $breadcrumbs = array();
+  public $layout = '//layouts/column1';
+  
+  public function filters() {
+    return array('accessControl');
+  }
+  
+  public function accessRules() {
+    return array(
+      array('deny', 'users' => array('?'))
+    );
+  }
 }
