30a93a42017073d1dba8767b5658bb6231278354
AlFaisal ElDajani <faisal.dajani@gmail.com>
2012-02-04 22:17:14 +0200

    Enabling all access to session controller so users can log into the system

--- protected/controllers/SessionController.php
+++ protected/controllers/SessionController.php
@@ -1,11 +1,21 @@
 <?php
 class SessionController extends Controller {
+  public function filters() {
+    return array('accessControl');
+  }
+  
+  public function accessRules() {
+    return array(
+      array('allow', 'users' => array('*'))
+    );
+  }
+  
   public function actionCreate() {
     if (Yii::app()->request->isPostRequest) {
       $identity = new UserIdentity($_POST['username'], $_POST['password']);
       if ($identity->authenticate()) {
         Yii::app()->user->login($identity);
-        $this->redirect(Yii::app()->getHomeUrl());
+        $this->redirect(Yii::app()->homeUrl);
       }
     }
     $this->render('create');
@@ -13,6 +23,6 @@ class SessionController extends Controller {
   
   public function actionDelete() {
     Yii::app()->user->logout(true);
-    $this->redirect(Yii::app()->getHomeUrl());
+    $this->redirect(Yii::app()->homeUrl);
   }
 }
